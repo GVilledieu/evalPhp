652eee27d158f9387bb9d5dc3856f4259cd24d02
GVilledieu <guillaume.villedieu@gmail.com>
2023-03-24 18:39:29 +0100

    Favoris

--- VoirFavoris.php
+++ VoirFavoris.php
@@ -1,23 +1,51 @@
 <?php
+session_start();
+require('./class/database.php');
+
+$database = new Database();
+// connexion bdd
+$pdo = $database->connectDb();
+
+// create select requete
+//$result = $database->select($pdo, '*', 'user', ['email', $_SESSION['email']]);
+
+$result = $database->selectFavorites($pdo);
+// formalisation du résultat
+$result = $result->fetchAll();
 ?>
-<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mes favoris</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Mes favoris</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-<div class="container bg-warning p-3 mt-5">
-    <?php
-    echo isset($_GET['error']) ? '<div data-bs-dismiss="3000" class="alert alert-danger alert-dismissible fade show" role="alert">
-                      <strong>Voici mon erreur!</strong> '.$_GET['error'].'
-                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>' : '';
-    ?>
-    <h1>Terre NBN</h1>
+<div class="container bg-secondary p-3 mt-5">
+      <h1>Mes favoris</h1>
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </html>
+
+<?php
+    echo '<div class="card-body">
+                        <div class="row">';
+    foreach ($result as $key => $value) {
+        echo '<div class="col-md-4 m-5">
+                        <div class="card">'
+            . '<h1 class="m-2">' . $value['title'] . '</h1>'
+            . '<h6 class="m-2">' . $value['description'] . '</h6>' .
+            '<p class="text-danger m-5">' .
+            $value['price'] . ' €
+                                </p>' .
+            '<p class="text-success m-5">' .
+            $value['address'] . '
+                                   </p>' .
+            '</div>
+                      </div>';
+
+        echo '</div>
+            </div>';
+}
+?>
