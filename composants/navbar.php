d2e47f5348fbf00b76ed6fccb07f7285872ca173
Kassir TIDJANI <kassirtidjani97@gmail.com>
2023-03-31 18:55:52 +0200

    ajout navbar

--- composants/navbar.php
+++ composants/navbar.php
@@ -14,29 +14,25 @@
     
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="Inscription.php">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand" href="Inscription.php"></a>
+        <img src="img/couette.jpg" alt="Bootstrap" width="80" height="80" class ="rounded-circle">
+        
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+              <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Inscription</a>
+              <a class="nav-link" href="inscription.php">Inscription</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Profil</a>
+              <a class="nav-link" href="">Profil</a>
             </li>
             <li class="nav-item">
               <a href="inscription.php" class="btn btn-outline-danger">DECONNEXION</a>
             </li>
           </ul>
-            <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+        
         </div>
       </div>
     </nav>
--- inscription.php
+++ inscription.php
@@ -1,40 +1,37 @@
+<?php 
+require("composants/navbar.php"); 
+require('composants/form.php');
+    $form = new Form();
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
-  <body class="p-3 m-0 border-0 bd-example">
-
-    <!-- Example Code -->
-    
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex" id="navbarNav" >
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Inscription</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Profil</a>
-            </li>
-            <li class="nav-item flex-row-reverse">
-              <a href="" class="btn btn-outline-success">Inscription</a>
-              <a href="navbar.php" class="btn btn-outline-success">CONNEXION</a>
-            </li>
-          </ul>
-            
-        </div>
-      </div>
-    </nav>
+  <body>
+    <div class="container bg-warning p-3 mt-5">
+            <?php 
+              echo isset($_GET['error']) ? '<div data-bs-dismiss="3000" class="alert alert-danger alert-dismissible fade show" role="alert">
+                      <strong>Voici mon erreur!</strong> '.$_GET['error'].'
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>' : ''; 
+            ?>
+            <h1>Facebook ou presque</h1>
+            <form class="row" action="/back-end/index.php" method="post">
+                <?php 
+                echo $form->Input("4", "nom", "Votre nom", "text", "Entrer un nom", $_GET['nom'] ?? '');
+                echo $form->Input("4", "prenom", "Votre prenom", "text", "Entrer un prenom", $_GET['prenom'] ?? '');
+                echo $form->Input("4", "email", "Votre email", "email", "Entrer un email", $_GET['email'] ?? '');
+                echo $form->Input("4", "telephone", "Votre téléphone", "tel", "Entrer un téléphone", $_GET['telephone'] ?? '');
+                echo $form->Input("4", "password", "Votre mot de passe", "password", "Entrer un mot de passe", '');
+                echo $form->Input("4", "password2", "Votre confirmation de mot de passe", "password", "Entrer un mot de passe", '');
+                echo $form->Input("4", "Envoyer", "Envoyer", "submit", "", 'Envoyer');
+                ?>
+            </form>
+    </div>    
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</html>
