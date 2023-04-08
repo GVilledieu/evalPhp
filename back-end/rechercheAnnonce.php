6f6315d50feaa01072dd94ec4a5a36ec9e6bda65
GVilledieu <guillaume.villedieu@gmail.com>
2023-04-08 11:31:19 +0200

    Ajouter un favoris

--- back-end/rechercheAnnonce.php
+++ back-end/rechercheAnnonce.php
@@ -17,21 +17,34 @@ else{
 echo ' <h1 class="text-white">Résultat de votre recherche pour '.$ville.'</h1>
       <div class="container m-3">
             <div class="row">';
-foreach ($resultVille as $key => $value) {
+    foreach ($resultVille as $key => $value) {
 
-    echo '<div class="col-sm-3 mb-3 mb-sm-0">
-                <div class="card bg-success">
+        echo '<div class="col-sm-3 mb-3 mb-sm-0">
+                <div class="card bg-success h-100 ">
                   <div class="card-body">
                     <h5 class="card-title">' . $value['title'] . '</h5>
-                    <p class="card-text">' . $value['description'] . '</p>
-                    <p> '.$value['price'] . '</p>
-                    <p class="text"> '.$value['address'] . '</p>
-                    <a href="#" class="btn text-white" style="background-color:#2A2A2A;">Voir l&#x2019;annonce</a>
+                    <p class="card-text">' . $value['address'] . '</p>
+                    <a href="#" class="btn text-white" style=background-color:#2A2A2A;">Voir l&#x2019;annonce</a>';
+        $mysqli = new mysqli("localhost", "root", "", "evalPhp");
+        $id_user=$_SESSION['id_user'];
+        $id_ad=$value["id_ad"];
+        $verify = mysqli_query($mysqli,"SELECT `id_user`, `id_ad` FROM `favorite` WHERE id_user=$id_user AND `id_ad` =$id_ad");
+        $rownum = mysqli_num_rows($verify);
+        if ($rownum < 1) {
+            echo '
+                        <form action="http://localhost/evalPhp/back-end/ajouterFavoris.php" method="post">
+                        <input type="hidden" name="id_ad" value="' .$value["id_ad"] . ' ">
+                            <input type="submit" class="btn btn-warning mt-3" value="Ajouter aux favoris">
+                           </form>';
+        } echo '
                   </div>
                 </div>
               </div>
-            ';}}
+            ;';
+
+
+    }
 
 echo ('<div class="container mt-3">
             <input class="btn btn-secondary" type="button" value="RETOUR" onClick="history.back();">
-      </div>');
+      </div>');}
